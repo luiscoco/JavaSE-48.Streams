8d8ad2fb70a6edeab47a0caaaa6b818adbcc5b18
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2023-10-18 22:41:55 +0200

    Update StreamExampleMapping.java

--- StreamExampleMapping.java
+++ StreamExampleMapping.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class StreamExample {
+public class StreamExampleMapping {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
 
